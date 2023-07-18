6c9e24c12173c4e1ace0619d9941c54b738e6f67
RicardoZon <337875754@qq.com>
2023-07-18 15:27:46 +0800

    ToolsTest

---
+++ Tools2/Sbaseline3demo.py
@@ -0,0 +1,15 @@
+import gymnasium as gym
+
+from stable_baselines3 import A2C
+
+model = A2C("MlpPolicy", "CartPole-v1").learn(10000)
+
+vec_env = model.get_env()
+obs = vec_env.reset()
+for i in range(1000):
+    action, _state = model.predict(obs, deterministic=True)
+    obs, reward, done, info = vec_env.step(action)
+    vec_env.render("human")
+    # VecEnv resets automatically
+    # if done:
+    #   obs = vec_env.reset()
