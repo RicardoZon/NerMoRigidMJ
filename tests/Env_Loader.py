abae0b38dd8fa19ff3188d5bd480998438764247
RicardoZon <337875754@qq.com>
2023-07-21 14:01:45 +0800

    test

--- tests/Env_Loader.py
+++ tests/Env_Loader.py
@@ -1,8 +1,8 @@
 import mujoco.viewer as viewer
 import mujoco
 
-m = mujoco.MjModel.from_xml_path("../models/dynamic_4l.xml")
-# m = mujoco.MjModel.from_xml_path("../models/fl_single.xml")
+# m = mujoco.MjModel.from_xml_path("../models/dynamic_4l.xml")
+m = mujoco.MjModel.from_xml_path("../models/fl_single.xml")
 # m = mujoco.MjModel.from_xml_path("../models/tail_assets/tail_")
 d = mujoco.MjData(m)
 # viewer.launch(m, d)
